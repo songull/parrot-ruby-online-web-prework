f5e6f7a7cde30c3462a77eba1351d62ac9f60ed2
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-19 02:50:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = "squeak")
+def parrot(phrase = "squawk")
   phrase
+end
+def parrot(phrase = "squawk")
+  puts phrase
 end
